1f89cb1eaf1cf2538575d83b1b5b8c661af020bf
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-10-08 15:04:02 +0500

    another try to push new version to version-2 branch

--- PSP_course_Exercise_Week1_7/Program.cs
+++ PSP_course_Exercise_Week1_7/Program.cs
@@ -18,36 +18,44 @@ namespace PSP_course_Exercise_Week1_7
                 *Otherwise, show an error message in red.
              */
 
-            bool isPasswordMatch = false; ;
+            ////version 2.0
+
+            bool isPasswordMatch = false;
+            string password1, password2 = null;
             do
             {
-                Console.Write("Enter a password: ");
-                string password1 = Console.ReadLine();
-
-                Console.WriteLine();
-
-                Console.Write("Enter the password again: ");
-                string password2 = Console.ReadLine();
-
-                Console.WriteLine();
-
-                if (password1 != string.Empty && password2 != string.Empty)
+                do
                 {
-                    if (password1.Equals(password2))
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("Passwords match !");
-                        isPasswordMatch = true;
-                        Console.ResetColor();
-                        Console.WriteLine();
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("Passwords don't match !");
-                        Console.ResetColor();
-                        Console.WriteLine();
-                    }
+                    Console.Write("Enter  password: ");
+                    password1 = Console.ReadLine();
+                    Console.WriteLine();
+
+
+                } while (password1.Equals(string.Empty));
+
+                do
+                {
+                    Console.Write("Enter the password again: ");
+                    password2 = Console.ReadLine();
+                    Console.WriteLine();
+
+
+                } while (password2.Equals(string.Empty));
+
+                if (password1.Equals(password2))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Passwords match !");
+                    isPasswordMatch = true;
+                    Console.ResetColor();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Passwords don't match !");
+                    Console.ResetColor();
+                    Console.WriteLine();
                 }
 
             } while (!isPasswordMatch);
