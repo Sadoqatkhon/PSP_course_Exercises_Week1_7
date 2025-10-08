592ed8578b7efa540979d7fccba617bb962608ef
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-10-08 15:26:42 +0500

    version provided by YouTube tutorial

--- PSP_course_Exercise_Week1_7/Program.cs
+++ PSP_course_Exercise_Week1_7/Program.cs
@@ -12,53 +12,59 @@ namespace PSP_course_Exercise_Week1_7
         {
 
             /*              
-                *Ask the user to enter their password, then request it again for confirmation.
-                *If the two entered passwords match,
-                *acknowledge with a message displayed in green. 
-                *Otherwise, show an error message in red.
+             * Ask user to enter password, and store
+             * Ask user to enter password again , and store
+             * Check if they are both contain something
+                *if so check if they are the same
+                    *if they are, print "Password match"
+                    *if they are not , print "Passwords don't match"
+                *If they are empty, print "Please enter a password"
              */
 
-            ////version 2.0
+            ////---------------------------
+            ////version of YouTube Tutorial
 
-            bool isPasswordMatch = false;
-            string password1, password2 = null;
-            do
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Enter password again");
+            string passwordC = Console.ReadLine();
+
+            if (!password.Equals(string.Empty))
             {
-                do
+                if (!passwordC.Equals(string.Empty))
                 {
-                    Console.Write("Enter  password: ");
-                    password1 = Console.ReadLine();
-                    Console.WriteLine();
+                    if (password.Length >= 6 && passwordC.Length >= 6)
+                    {
+                        if (password.Equals(passwordC))
+                        {
+                            Console.WriteLine("Password match");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Passwords do not match");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("please enter 6 or more characters");
+                    }
 
-
-                } while (password1.Equals(string.Empty));
-
-                do
-                {
-                    Console.Write("Enter the password again: ");
-                    password2 = Console.ReadLine();
-                    Console.WriteLine();
-
-
-                } while (password2.Equals(string.Empty));
-
-                if (password1.Equals(password2))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine("Passwords match !");
-                    isPasswordMatch = true;
-                    Console.ResetColor();
-                    Console.WriteLine();
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Passwords don't match !");
-                    Console.ResetColor();
-                    Console.WriteLine();
+                    Console.WriteLine("Enter password confirmation again");
                 }
-
-            } while (!isPasswordMatch);
+            }
+            else
+            {
+                Console.WriteLine("Enter password again");
+            }
         }
+
     }
+
 }
+
+
+
